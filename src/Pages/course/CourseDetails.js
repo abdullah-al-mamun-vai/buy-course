b6fff66bea8f8fdc0baf8e92d4edcf5b53b602aa
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-10-25 22:48:24 +0600

    details content added

--- src/Pages/course/CourseDetails.js
+++ src/Pages/course/CourseDetails.js
@@ -7,7 +7,16 @@ const CourseDetails = () => {
     console.log(details)
     return (
         <div>
-
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content ">
+                    <div className="w-7/12">
+                        <img className='w-full h-5/6' src={picture} alt="" />
+                        <h1 className="text-5xl font-bold capitalize">{name}</h1>
+                        <p className="py-6">{about}</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
